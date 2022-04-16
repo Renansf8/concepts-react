9738c35ad1c47fbdef43cd948128c268c9de7637
Renan Ferreira <renansantiago2013@hotmail.com>
2022-04-16 12:37:20 -0300

    create task only with a title

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -22,9 +22,11 @@ export function TaskList() {
       isComplete: false
     }
 
+    if (newTask.title === '') return alert('Adicione um título à tarefa')
+
     setTasks([...tasks, newTask]);
     setNewTaskTitle('');
-  }
+  } 
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
